49504fb249f417bc723a7507c0933a0a23b3f658
PmtGitTech <pmt.work28@gmail.com>
2026-06-08 12:05:08 +0530

    learned intsructions and operators

---
+++ CHAPTER 2/1_variables.c
@@ -0,0 +1,10 @@
+// here we are doing declaration and initialization diff. methos of variables...
+
+/*
+a+b=c
+in this "+ and "= are operator and a and b are operand... 
+
+We leaned:
+1. instructions and operators
+
+*/
